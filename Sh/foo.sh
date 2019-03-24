484f931837e5c6f3fcce8d2f1b016e4b0f6aeb8c
bnonni <bryanwnonni@gmail.com>
2019-03-24 11:24:54 -0400

    updates

--- Sh/foo.sh
+++ Sh/foo.sh
@@ -7,4 +7,9 @@ x=0 # initialization x = 0
 i=1
 while [ $i -le 3 ] #while(i<=3)
 do
-s=`expr $i \* $i`
+s=`expr $i \* $i` # s=i*i 
+x=`expr $s + $x` # x=(i*i)+x
+i=`expr $i + 1` # i=i+1
+done
+
+echo x=$x
